d7b8d817cac0340dcfda92dce5e8dab91ad14efd
Rosellyne <elijahroselyne@gmail.com>
2019-05-09 13:11:05 +0300

    add the paragraph to div card and the font color of green

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,15 @@ $(document).ready(function(){
         $(this).children(".hide").toggle(1200);
         $(this).children("img").toggle(1200);
     });
-});
 
-function sendForm() {
-    document.getElementById("myForm").style.display = "block";
-    var dataName = $("#number1").val();
-    alert("Thank you " + dataName + " your message has been received");
-    $('form').attr('action','new path');
-}
+    $(".card").hover(function(event){
+        $(".text").hide();
+        $(this).children(".text").toggle();
+    });
+    $("button").click(function(event) {
+        event.preventDefault();
+        var dataName = $("#number1").val();
+        alert(dataName + " we've received your message, thank you for getting in touch with us");
+      });
+    
+});
